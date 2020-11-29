280519c101d24e46623ae83747fe4454363a1e97
W4nS <75206727+W4nS@users.noreply.github.com>
2020-11-29 21:42:35 +0700

    Update index.js

--- index.js
+++ index.js
@@ -13,11 +13,10 @@ const tambahan = require("./lib/tambahan.js")
 const donasi = require("./lib/donasi.js");
 const info = require("./lib/info.js");
 /////////////////
-const BotName = 'REUZ BOT☄'; 
-const instagram = 'https://instagram.com/reuz.id'; 
-const telegram = 'https://t.me/reuzzz'; 
+const BotName = 'Wans BOT☄'; 
+const instagram = 'https://instagram.com/norman_wawan';  
 const kapanbotaktif = 'KETIKA HANYA DIAKTIFKAN'; 
-const youtube = 'https://www.youtube.com/c/reuz_id';
+const youtube = 'https://www.youtube.com/c/norman wawan';
 //const grupch1 = 'belum ada grup'; 
 //const grupch2 = 'belum ada grup' ; 
 const
